138ea1818a6f97236eaba4309d771cec9bf3909b
Phil <streicher1993@googlemail.com>
2021-12-01 06:33:54 +0100

    improvement 2021 1_2

--- src/aoc2021/aoc1.rs
+++ src/aoc2021/aoc1.rs
@@ -16,13 +16,11 @@ pub fn solve_part1(input: &str) -> u32 {
 #[aoc(day1, part2)]
 pub fn solve_part2(input: &str) -> u32 {
     let mut i = 0;
-    for (e1, e2) in input
+    for e in input
                 .lines()
                 .map(|s| i32::from_str(s.trim()).expect("parsing"))
-                .tuple_windows::<(_,_,_)>()
-                .map(|(e1, e2, e3)| e1+e2+e3)
-                .tuple_windows::<(_,_)>() {
-        if e2 > e1 {
+                .tuple_windows::<(_,_,_,_)>() {
+        if e.3 > e.0 {
             i += 1;
         }
     }
