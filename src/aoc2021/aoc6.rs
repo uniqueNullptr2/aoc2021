6640518412569e04b07aa7ddfbe8db873da14cf9
Phil <streicher1993@googlemail.com>
2021-12-06 13:40:23 +0100

    you spin my head right round

--- src/aoc2021/aoc6.rs
+++ src/aoc2021/aoc6.rs
@@ -1,63 +1,37 @@
-use std::collections::HashMap;
 use aoc_runner_derive::{aoc, aoc_generator};
 use std::str::FromStr;
+use std::collections::VecDeque;
 
-#[derive(Copy, Clone)]
-pub struct Fish {
-    timer: u8,
-}
-impl Fish {
-    pub fn new (timer: u8) -> Self {
-        Self{timer}
+
+pub fn generator_6(input: &str) -> VecDeque<usize> {
+    let mut arr = [0usize;9];
+    for n in input.split(',').map(|s| usize::from_str(s).unwrap()) {
+        arr[n] += 1;
     }
-    pub fn spawn(&mut self) -> Option<Self> {
-        if self.timer == 0 {
-            self.timer = 6;
-            return Some(Self{timer: 8});
-        }
-        self.timer -= 1;
-        None
-    }
+    VecDeque::from(arr)
 }
-#[aoc_generator(day6)]
-pub fn generator_6(input: &str) -> Vec<Fish> {
-    input.split(',').map(|s| u8::from_str(s).unwrap()).map(|n| Fish::new(n)).collect()
-}
+
 #[aoc(day6, part1)]
-pub fn solve_part1(input: &[Fish]) -> usize {
-    let mut c = input.len();
-    let mut map = HashMap::new();
-    for f in input {
-        c += count_helper(*f, 80, &mut map);
-    }
-    c
+pub fn solve_part1(input: &str) -> usize {
+    let mut vec = generator_6(input);
+    run_days(&mut vec, 80)
 }
 
 #[aoc(day6, part2)]
-pub fn solve_part2(input: &[Fish]) -> usize {
-    let mut c = input.len();
-    let mut map = HashMap::new();
-    for f in input {
-        c += count_helper(*f, 256, &mut map);
-    }
-    c
+pub fn solve_part2(input: &str) -> usize {
+    let mut vec = generator_6(input);
+    run_days(&mut vec, 256)
 }
 
+fn run_day(vec: &mut VecDeque<usize>) {
+    let n = vec.pop_front().unwrap();
+    *vec.get_mut(vec.len()-2).unwrap() += n;
+    vec.push_back(n);
+}
 
-fn count_helper(mut fish: Fish, days: u16, map: &mut HashMap<(u8,u16),usize>) -> usize {
-    let mut c = 0;
-    for i in 1..=days {
-        if let Some(f) = fish.spawn() {
-            c += 1;
-            if map.contains_key(&(f.timer, days-i)) {
-                c += map.get(&(f.timer, days-i)).unwrap();
-            } else {
-                let t = f.timer;
-                let cc = count_helper(f, days - i, map);
-                map.insert((t, days-i), cc);
-                c += cc;
-            }
-        }
+fn run_days(vec: &mut VecDeque<usize>, days: usize)  -> usize {
+    for _ in 0..days {
+        run_day(vec);
     }
-    c
+    vec.iter().sum()
 }
