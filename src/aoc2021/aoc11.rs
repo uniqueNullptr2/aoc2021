f81dff0b8bf4b52b3f2d5c39c36fea1bf3767a7f
uniqueNullptr2 <philipp.classen@protonmail.com>
2021-12-11 17:37:41 +0100

    not touching this again

--- src/aoc2021/aoc11.rs
+++ src/aoc2021/aoc11.rs
@@ -1,18 +1,17 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque};
 
+type Octo = i8;
 
-pub fn input_generator(input: &str) -> [u8; 100] {
+pub fn input_generator(input: &str) -> Vec<Octo> {
     input.lines()
         .map(|l| {
-            l.chars().map(|c| c.to_digit(10).unwrap() as u8)
+            l.chars().map(|c| c.to_digit(10).unwrap() as Octo)
         })
         .flatten()
-        .collect::<Vec<u8>>()
-        .try_into()
-        .unwrap()
+        .collect()
 }
 
-pub fn solve_part1(_vec: &mut [u8;100]) -> usize {
+pub fn solve_part1(_vec: &mut [Octo]) -> usize {
     let mut c = 0;
     for _ in 0..100 {
         c += flash_octos(_vec);
@@ -20,10 +19,11 @@ pub fn solve_part1(_vec: &mut [u8;100]) -> usize {
     c
 }
 
-pub fn solve_part2(_vec: &mut [u8;100]) -> usize {
+pub fn solve_part2(_vec: &mut [Octo]) -> usize {
     let mut i = 0;
+    let l = _vec.len();
     loop {
-        if flash_octos( _vec) == 100 {
+        if flash_octos( _vec) == l {
             break;
         }
         i += 1;
@@ -31,13 +31,14 @@ pub fn solve_part2(_vec: &mut [u8;100]) -> usize {
     i+1
 }
 
-fn flash_octos(octos: &mut [u8;100]) -> usize {
-    let mut stack = VecDeque::<usize>::new();
-    for (i,octo) in octos.iter_mut().enumerate() {
-            *octo += 1;
-            if *octo == 10 {
-                stack.push_back(i);
-            }
+fn flash_octos(octos: &mut [Octo]) -> usize {
+    let mut stack = VecDeque::new();
+    let len = (octos.len() as f32).sqrt() as usize;
+    for (i, octo) in octos.iter_mut().enumerate() {
+        *octo += 1;
+        if *octo == 10 {
+            stack.push_back(i);
+        }
     }
     let mut c = 0;
     while let Some(i) = stack.pop_back() {
@@ -47,10 +48,8 @@ fn flash_octos(octos: &mut [u8;100]) -> usize {
             if *octo > 9 {
                 c += 1;
                 *octo = 0;
-                for e in get_octos(i) {
-                    if let Some(x) = e.map(|e| octos.get(e).map(|_| e)).flatten() {
-                        stack.push_back(x);
-                    }
+                for i in get_octos(i, len).into_iter().filter_map(|f| f) {
+                    stack.push_back(i);
                 }
             }
         }
@@ -58,21 +57,21 @@ fn flash_octos(octos: &mut [u8;100]) -> usize {
     c
 }
 
-fn get_octos(i: usize) -> [Option<usize>;8] {
-    let x = i % 10;
-    let y = (i - x) / 10;
-    let xm1 = x.checked_sub(1).filter(|n| *n < 10);
-    let xp1 = x.checked_add(1).filter(|n| *n < 10);
-    let ym1 = y.checked_sub(1).filter(|n| *n < 10);
-    let yp1 = y.checked_add(1).filter(|n| *n < 10);
+fn get_octos(i: usize, l: usize) -> [Option<usize>;8] {
+    let x = i % l;
+    let y = (i-x)/l;
+    let ym1 = y.checked_sub(1).filter(|n| *n < l);
+    let yp1 = y.checked_add(1).filter(|n| *n < l);
+    let xm1 = x.checked_sub(1).filter(|n| *n < l);
+    let xp1 = x.checked_add(1).filter(|n| *n < l);
     [
-        ym1.and(xm1).map(|_| i-11),
-        ym1.map(|_| i-10),
-        ym1.and(xp1).map(|_| i-9),
-        xm1.map(|_| i-1),
-        xp1.map(|_| i+1),
-        yp1.and(xm1).map(|_| i+9),
-        yp1.map(|_| i+10),
-        yp1.and(xp1).map(|_| i+11),
+        ym1.zip(xm1).map(|(i,e)| i*l+e),
+        ym1.map(|n|n*l+x),
+        ym1.zip(xp1).map(|(i,e)| i*l+e),
+        xm1.map(|n|y*l+n),
+        xp1.map(|n|y*l+n),
+        yp1.zip(xm1).map(|(i,e)| i*l+e),
+        yp1.map(|n|n*l+x),
+        yp1.zip(xp1).map(|(i,e)| i*l+e),
     ]
 }
